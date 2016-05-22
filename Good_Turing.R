e39fdb5e42d8f5d5ddb9cfacb224250b38123076
pkoukos <pkoukos2009@hotmail.com>
2016-05-22 16:46:12 +0200

    Update source and LICENCE to current year

--- Good_Turing.R
+++ Good_Turing.R
@@ -1,6 +1,6 @@
 ################################################################################
 ##                                                                            ##
-## Copyright (c) 2013 - 2014, Panagiotis I. Koukos                            ##
+## Copyright (c) 2013 - 2016, Panagiotis I. Koukos                            ##
 ## All rights reserved.                                                       ##
 ##                                                                            ##
 ## Redistribution and use in source and binary forms, with or without         ##
